da71a1b6fa12d7403b7b25981bfe766316564a30
tomas <tomascostapp@gmail.com>
2023-11-13 19:06:20 -0300

    Codigo optimizado

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Parcial-2-Tomas-Costa
 
-# Nombre: Tomás
-# Apellido: Costa
+# Nombre: Tomás Costa
+# Legajo:
+# Email: tomascostapp@gmail.com 
 # DNI: 44.625.800
+
+# De que trata el proyecto
+
+# Como lo resolvimos (Alto nivel)
+
+# Como correrlo
---
+++ main.py
@@ -0,0 +1,33 @@
+def test(adn):
+    def esMutante(secuencia):
+        return any(secuencia[i:i+4] == secuencia[i]*4 for i in range(len(secuencia)-3))
+
+    # Filas
+    if any(esMutante(fila) for fila in adn):
+        return True
+
+    # Columnas
+    if any(esMutante(''.join(adn[i][j] for i in range(len(adn)))) for j in range(len(adn[0]))):
+        return True
+
+    # Diagonales
+    if any(esMutante(''.join(adn[i+k][j+k] for k in range(4))) for i in range(len(adn)-3) for j in range(len(adn[0])-3)):
+        return True
+
+    if any(esMutante(''.join(adn[i-k][j+k] for k in range(4))) for i in range(3, len(adn)) for j in range(len(adn[0])-3)):
+        return True
+
+    return False
+
+
+adn = ["ATGCGA", 
+       "CDGTGC", 
+       "TTATST", 
+       "AGAAGG", 
+       "CCCDTA", 
+       "TCACTG"]
+
+if test(adn):
+    print("La persona es mutante.")
+else:
+    print("La persona no es mutante.")
